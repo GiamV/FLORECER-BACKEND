a2032aa6e1415c35576752dca1ea7a81d67cfa8f
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2024-11-30 11:46:26 -0500

    g

--- Florecer/src/main/java/com/idat/florecer/controller/productoController.java
+++ Florecer/src/main/java/com/idat/florecer/controller/productoController.java
@@ -1,7 +1,11 @@
 package com.idat.florecer.controller;
 
+
+
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,10 +21,13 @@ import com.idat.florecer.entity.Producto;
 import com.idat.florecer.service.IProductoService;
 import com.idat.florecer.serviceR.CategoriaServiceIm;
 
+
 @CrossOrigin(origins= {"http://localhost/4200"})
 @RestController
 @RequestMapping("/producto")
 public class productoController {
+    // Crear un logger para esta clase
+    private static final Logger logger = LoggerFactory.getLogger(productoController.class);	
 	
 	@Autowired
 	private IProductoService productoService;
@@ -30,18 +37,24 @@ public class productoController {
 	//LISTAR PRODUCTOS
 	@GetMapping("/productos")
 	public List<Producto> listar(){
+		logger.debug("Se ha llamado al endpoint /productos");
+        logger.info("Enviando listado de Productos");
 		return productoService.findAll();
 	}
 	
 	//LISTAR PRODUCTOS
 		@GetMapping("/productoscat/{id}")
 		public List<Producto> listarbycodcat(@PathVariable Long id){
+			logger.debug("Se ha llamado al endpoint /productoscat/"+id);
+	        logger.info("Enviando listado de Productos");
 			return productoS.findByCate(id);
 		}
 	
 	//BUSCAR PRODUCTO POR ID
 	@GetMapping("/producto/{id}")
 	public Producto producto (@PathVariable Long id) {
+		logger.debug("Se ha llamado al endpoint /producto/"+id);
+        logger.info("Enviando busqueda de producto");
 		return productoService.findById(id);
 	}
 	
@@ -49,6 +62,8 @@ public class productoController {
 	@PostMapping("/productonew")
 	public Producto productonew(@RequestBody Producto producto) {
 		productoService.save(producto);
+		logger.debug("Se ha llamado al endpoint /productonew");
+        logger.info("Enviando metodo Post");
 		return productoService.findById(producto.getIdProducto()); 
 	}
 	
@@ -64,7 +79,8 @@ public class productoController {
 		productoActual.setPrecio(producto.getPrecio());
 		productoActual.setPrecioCompra(producto.getPrecioCompra());
 		productoActual.setStock(producto.getStock());
-	
+		logger.debug("Se ha llamado al endpoint /productoupdate"+id);
+        logger.info("Enviando metodo Put");
 		productoService.save(productoActual);
 		return productoService.findById(id);
 	}
@@ -83,6 +99,8 @@ public class productoController {
 		}else {
 			productoActual.setEstado(1);
 		}
+		logger.debug("Se ha llamado al endpoint /productodelete"+id);
+        logger.info("Enviando metodo Delete");
 		
 		productoService.save(productoActual);
 	}
